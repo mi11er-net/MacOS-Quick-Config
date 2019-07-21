8699bff5794004842bf8a65fb68082e3eaedbf46
Matthew Miller <matthew@mi11er.net>
2019-07-20 20:45:38 -0400

    refactor: 💡 Don't track version file

--- quick_config/__init__.py
+++ quick_config/__init__.py
@@ -2,4 +2,7 @@
     Package Init
 """
 
-from ._version import __version__
+try:
+    from ._version import __version__
+except ImportError:
+    __version__ = "0.0.0-DEV"
--- quick_config/_version.py
+++
@@ -1,5 +0,0 @@
-"""
-  Version info
-"""
-
-__version__ = "v1.1.2-dev.24.uncommitted+fork-app.a0677a1"
